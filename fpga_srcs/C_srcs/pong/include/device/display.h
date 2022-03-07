9fd46c094124888cb937c008b889121210e11447
Atharva Lele <itsatharva@gmail.com>
2022-03-06 16:50:24 -0800

    fpga: display_driver: implement basic display functions
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

--- fpga_srcs/C_srcs/pong/include/device/display.h
+++ fpga_srcs/C_srcs/pong/include/device/display.h
@@ -14,6 +14,10 @@
 #define VGA_WRITE_ENABLE_OFFSET         0x0
 #define VGA_PIXEL_ADR_OFFSET            0x1
 #define VGA_PIXEL_DAT_OFFSET            0x2
+#define VGA_PIXEL_HL_INC_OFFSET         0x3
+#define VGA_PIXEL_HL_DEC_OFFSET         0x4
+#define VGA_PIXEL_VL_INC_OFFSET         0x5
+#define VGA_PIXEL_VL_DEC_OFFSET         0x6
 
 #define VGA_ROWS                        640
 #define VGA_COLS                        480
@@ -22,6 +26,11 @@
 #define PIXEL_OFF                       0x0
 
 void display_init();
+void display_clear();
+void display_fill();
+
 void draw_pixel(u16_t row, u16_t col,  u08_t value);
-void create_divider();
+void display_draw_hline(s16_t x0, s16_t y0, u16_t width, u08_t val);
+void display_draw_vline(s16_t x0, s16_t y0, u16_t height, u08_t val);
+
 #endif //__DISPLAY_H__
--- fpga_srcs/C_srcs/pong/src/device/display.c
+++ fpga_srcs/C_srcs/pong/src/device/display.c
@@ -6,22 +6,46 @@
 
 #include "device/display.h"
 
+/* Functions to accelerate drawing of horizontal / vertical lines */
+static inline void draw_pixel_auto_inc_hline(u08_t val)
+{
+    WRITE_REG(VGA_BASE_ADDR, VGA_PIXEL_HL_INC_OFFSET, val);
+}
+
+static inline void draw_pixel_auto_dec_hline(u08_t val)
+{
+    WRITE_REG(VGA_BASE_ADDR, VGA_PIXEL_HL_DEC_OFFSET, val);
+}
+
+static inline void draw_pixel_auto_inc_vline(u08_t val)
+{
+    WRITE_REG(VGA_BASE_ADDR, VGA_PIXEL_VL_INC_OFFSET, val);
+}
+
+static inline void draw_pixel_auto_dec_vline(u08_t val)
+{
+    WRITE_REG(VGA_BASE_ADDR, VGA_PIXEL_VL_DEC_OFFSET, val);
+}
+
 void display_init() 
 {
     /* Enable write for frame buffer */
-    /* From testing we can leave it enabled */
     WRITE_REG(VGA_BASE_ADDR, VGA_WRITE_ENABLE_OFFSET, 1);
-
-    for(int i = 0; i < 640; i++)
-        for(int j = 0; j < 480; j++)
-            draw_pixel(i, j, PIXEL_OFF);
 }
 
+void display_deinit()
+{
+    /* Enable write for frame buffer */
+    WRITE_REG(VGA_BASE_ADDR, VGA_WRITE_ENABLE_OFFSET, 0);
+}
+
+/* Calculate which pixel to turn ON */
 static inline u32_t calculate_frame_addr(u16_t x, u16_t y)
 {
     return ((y * VGA_ROWS) + x);
 }
 
+/* Write address, and pixel */
 void draw_pixel(u16_t x, u16_t y, u08_t value) 
 {
     u32_t addr = calculate_frame_addr(x, y);
@@ -29,8 +53,39 @@ void draw_pixel(u16_t x, u16_t y, u08_t value)
     WRITE_REG(VGA_BASE_ADDR, VGA_PIXEL_DAT_OFFSET, value);
 }
 
-void create_divider()
+/* Clear display */
+void display_clear()
 {
-    for (int i = 0; i < 640; i++)
-        draw_pixel(i, 240, PIXEL_ON);
+    volatile u16_t i, j;
+
+    draw_pixel(0, 0, 0);
+    
+    for (i = 0; i < 640; i++) {
+        for (j = 0; j < 480; j++) {
+            draw_pixel_auto_inc_hline(0);
+        }
+    }
+}
+
+/* Draw horizontal line */
+void display_draw_hline(s16_t x0, s16_t y0, u16_t width, u08_t val)
+{
+    volatile u16_t w = width - 1;
+
+    draw_pixel(x0, y0, val);
+
+    while (w--) {
+        draw_pixel_auto_inc_hline(val);
+    }
+}
+
+/* Draw vertical line */
+void display_draw_vline(s16_t x0, s16_t y0, u16_t height, u08_t val)
+{
+    volatile u16_t h = height - 1;
+    draw_pixel(x0, y0, val);
+
+    while (h--) {
+        draw_pixel_auto_inc_vline(val);
+    }
 }
