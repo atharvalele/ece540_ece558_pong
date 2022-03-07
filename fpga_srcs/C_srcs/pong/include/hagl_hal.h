6da6afdb3417fa1ee99377a385cb910c4e9c7466
Atharva Lele <itsatharva@gmail.com>
2022-03-06 16:50:46 -0800

    fpga: display_driver: add HAGL HAL basic implementation
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

---
+++ fpga_srcs/C_srcs/pong/include/hagl_hal.h
@@ -0,0 +1,30 @@
+#ifndef __HAGL_HAL_H__
+#define __HAGL_HAL_H__
+
+/**
+ * HAGL library HAL provider
+ * 
+ * Author: Atharva Lele <atharva@pdx.edu>
+ */
+
+#include "psp_types.h"
+#include <bitmap.h>
+
+typedef u16_t color_t;
+
+#define DISPLAY_WIDTH       640
+#define DISPLAY_HEIGHT      480
+#define DISPLAY_DEPTH       100
+
+#define HAGL_HAS_HAL_INIT
+#define HAGL_HAS_HAL_CLEAR_SCREEN
+#define HAGL_HAS_HAL_HLINE
+#define HAGL_HAS_HAL_VLINE
+
+bitmap_t *hagl_hal_init(void);
+void hagl_hal_put_pixel(int16_t x0, int16_t y0, color_t color);
+void hagl_hal_clear_screen();
+void hagl_hal_hline(int16_t x0, int16_t y0, uint16_t width, color_t color);
+void hagl_hal_vline(int16_t x0, int16_t y0, uint16_t height, color_t color);
+
+#endif
---
+++ fpga_srcs/C_srcs/pong/src/hagl_hal.c
@@ -0,0 +1,30 @@
+#include "hagl_hal.h"
+#include "device/display.h"
+
+
+bitmap_t *hagl_hal_init(void)
+{
+    display_init();
+    return NULL;
+}
+
+void hagl_hal_clear_screen(void)
+{
+    display_clear();
+}
+
+void hagl_hal_put_pixel(int16_t x0, int16_t y0, color_t color)
+{
+    draw_pixel(x0, y0, 0xF);
+}
+
+void hagl_hal_hline(int16_t x0, int16_t y0, uint16_t width, color_t color)
+{
+    display_draw_hline(x0, y0, width, color);
+}
+
+void hagl_hal_vline(int16_t x0, int16_t y0, uint16_t height, color_t color)
+{
+    display_draw_vline(x0, y0, height, color);
+}
+
